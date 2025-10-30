a77726e2595cab0fa686cee089665481720500a3
Your Name <arthurlopes25072005@gmail.com>
2025-10-30 10:20:17 -0300

    feat: criado o menu basico do sistema

--- trabalho/src/Main.java
+++ trabalho/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-
+        Scanner scanner = new Scanner(System.in);
+        int opcao = 0;
+        while (opcao != -1){
+            System.out.println("Digite 1: para criar e adicionar livro ");
+            System.out.println("Digite 2: para remover livro pelo titulo");
+            System.out.println("Digite 3: para fazer o bubble sort");
+            System.out.println("Digite 4: para fazer o insertion sort");
+            System.out.println("Digite 5: para fazer o quick sort");
+            System.out.println("Digite -1 para sair");
+        }
     }
 }
