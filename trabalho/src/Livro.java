084f244270013494b87fdf61cd21353a73a8c6c0
Your Name <arthurlopes25072005@gmail.com>
2025-10-30 10:05:44 -0300

    feat: criando a entidade de livro

---
+++ trabalho/src/Livro.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Livro {
+    private String titulo;
+    private Date anoPublicacao;
+    private int numeroDePagina;
+    public Livro(String titulo,Date anoPublicacao,int numeroDePagina){
+        this.titulo=titulo;
+        this.anoPublicacao = anoPublicacao;
+        this.numeroDePagina = numeroDePagina
+    }
+}
