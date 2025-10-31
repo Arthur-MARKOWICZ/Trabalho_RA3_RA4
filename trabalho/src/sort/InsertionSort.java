2c9b05913d3fbb2d27063f8f10c461f2b43fa4e5
Andre-benato <andregbenato@outlook.com>
2025-10-31 10:25:07 -0300

    Adicionando Sorts

--- trabalho/src/sort/InsertionSort.java
+++ trabalho/src/sort/InsertionSort.java
@@ -1,4 +1,42 @@
 package sort;
 
+import java.util.Scanner;
+
 public class InsertionSort {
+
+    public static void insertionSort(int[] vetor) {
+        for (int i = 1; i < vetor.length; i++) {
+            int chave = vetor[i];
+            int j = i - 1;
+            while (j >= 0 && vetor[j] > chave) {
+                vetor[j + 1] = vetor[j];
+                j--;
+            }
+            vetor[j + 1] = chave;
+        }
+    }
+
+    public static void exibirVetor(int[] vetor) {
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o tamanho do vetor: ");
+        int n = scanner.nextInt();
+        int[] vetor = new int[n];
+        System.out.println("Digite os elementos do vetor:");
+        for (int i = 0; i < n; i++) {
+            vetor[i] = scanner.nextInt();
+        }
+        System.out.println("\nVetor antes da ordenação:");
+        exibirVetor(vetor);
+        insertionSort(vetor);
+        System.out.println("\nVetor após a ordenação (Insertion Sort):");
+        exibirVetor(vetor);
+        scanner.close();
+    }
 }
--- trabalho/src/sort/QuickSort.java
+++ trabalho/src/sort/QuickSort.java
@@ -1,4 +1,56 @@
 package sort;
 
+import java.util.Scanner;
+
 public class QuickSort {
+
+    public static void quickSort(int[] vetor, int inicio, int fim) {
+        if (inicio < fim) {
+            int indicePivo = particionar(vetor, inicio, fim);
+            quickSort(vetor, inicio, indicePivo - 1);
+            quickSort(vetor, indicePivo + 1, fim);
+        }
+    }
+
+    public static int particionar(int[] vetor, int inicio, int fim) {
+        int pivo = vetor[fim];
+        int i = inicio - 1;
+        for (int j = inicio; j < fim; j++) {
+            if (vetor[j] <= pivo) {
+                i++;
+                int temp = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = temp;
+            }
+        }
+        int temp = vetor[i + 1];
+        vetor[i + 1] = vetor[fim];
+        vetor[fim] = temp;
+        return i + 1;
+    }
+
+    public static void exibirVetor(int[] vetor) {
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o tamanho do vetor: ");
+        int n = scanner.nextInt();
+        int[] vetor = new int[n];
+        System.out.println("Digite os elementos do vetor:");
+        for (int i = 0; i < n; i++) {
+            vetor[i] = scanner.nextInt();
+        }
+        System.out.println("\nVetor antes da ordenação:");
+        exibirVetor(vetor);
+        quickSort(vetor, 0, vetor.length - 1);
+        System.out.println("\nVetor após a ordenação (Quick Sort):");
+        exibirVetor(vetor);
+        scanner.close();
+    }
 }
+
